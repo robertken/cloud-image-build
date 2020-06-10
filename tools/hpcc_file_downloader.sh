f801468734d059794d3c903e5a3c4aaf7f62a980
robertken <robbykennedy@yahoo.com>
2020-06-10 15:57:52 -0400

    minor tweaks

--- tools/hpcc_file_downloader.sh
+++ tools/hpcc_file_downloader.sh
@@ -12,15 +12,15 @@ fi
 
 mkdir -p ../hpcc-systems/bionic
 
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/platform/hpccsystems-platform-community_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-couchbaseembed_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-javaembed_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-kafka_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-memcached_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-mysqlembed_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-redis_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-rembed_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-spark_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-sqlite3embed_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-sqs_${HPCC_VERSION}-1bionic_amd64.deb
-wget -P ../hpcc-systems/bionic/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-v8embed_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/platform/hpccsystems-platform-community_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-couchbaseembed_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-javaembed_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-kafka_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-memcached_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-mysqlembed_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-redis_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-rembed_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-spark_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-sqlite3embed_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-sqs_${HPCC_VERSION}-1bionic_amd64.deb
+wget -P ../hpcc-systems/bionic_${HPCC_VERSION}/ https://d2wulyp08c6njk.cloudfront.net/releases/CE-Candidate-${HPCC_VERSION}/bin/plugins/hpccsystems-plugin-v8embed_${HPCC_VERSION}-1bionic_amd64.deb
